e85ad4909b4610b0f0695c3307e63002888d0515
dismas <afubwa@gmail.com>
2023-01-14 22:32:59 +0400

    First Class Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = (scubersDrivers) => {
+    return scubersDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (scubersDrivers) => {
+  return scubersDrivers.slice(scubersDrivers.length - 2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function (someFare) {
+      return num * someFare;
+};
+}
+
+const fareDoubler = (num) => createFareMultiplier(2)(num);
+
+const fareTripler = (num) => createFareMultiplier(3)(num);
+
+function selectDifferentDrivers(scubersDrivers, aFunction) {
+    return aFunction(scubersDrivers);
+}
